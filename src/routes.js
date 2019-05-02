d105fa723b555d851773052a276ae75fb20e87da
IannNascimento <49248138+IannNascimento@users.noreply.github.com>
2019-05-02 19:10:58 -0300

    Pimeira modificação

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Notifications from "views/Components/Notifications.jsx";
+
+var dashRoutes = [
+{
+  path: "/notifications",
+  name: "Notifications",
+  mini: "N",
+  component: Notifications,
+  layout: "/admin"
+}
+];
+export default dashRoutes;
